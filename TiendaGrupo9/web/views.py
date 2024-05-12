a6ebacc4111a419528e399730354d24e9ed5c3ca
MicaBelenrSoria <micabelenrs@gmail.com>
2024-05-12 17:10:21 -0300

    Se agrega  'el context ' en la funcion index del archivo views de la carpeta web

--- TiendaGrupo9/web/views.py
+++ TiendaGrupo9/web/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
 def index(request):
-    return render(request,'web/index.html')
+
+    # Accede a la base de datos atraves de los modelos 
+    context = {
+        'nombre': 'Comunidad Tengnologica', #Se le puede cambiar con el nombre ue ustedes quieren
+        'fecha_hora': datetime.datetime.now() 
+    }
+
+    return render(request,'web/index.html', context)
