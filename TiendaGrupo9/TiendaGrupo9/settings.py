7abce38ee17ebe5beb1d9aa05ba5e14462820014
MicaBelenrSoria <micabelenrs@gmail.com>
2024-05-28 13:32:13 -0300

    CONECTO BASE DE DATOS

--- TiendaGrupo9/TiendaGrupo9/settings.py
+++ TiendaGrupo9/TiendaGrupo9/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'TiendaGrupo9.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cac_2024_24309',
+        'USER': 'postgres',
+        'PASSWORD': 'Amado010',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
