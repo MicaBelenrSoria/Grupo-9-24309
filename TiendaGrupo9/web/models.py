88e86fb8be6dbd23b799806a54b531c46e4cdbc5
MicaBelenrSoria <micabelenrs@gmail.com>
2024-05-29 23:47:39 -0300

    Models.py + class Producto

--- TiendaGrupo9/web/models.py
+++ TiendaGrupo9/web/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100, verbose_name= "Nombre")
+    descripcion = models.CharField(max_length=200, verbose_name= "Descripcion")
+    precio = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Precio")
+    imagen_url = models.URLField(verbose_name="Imagen")
+
+ #Crear Migraciones 
+ #python manage.py makemigrations web   
+
+ #Ejecutar migracion
