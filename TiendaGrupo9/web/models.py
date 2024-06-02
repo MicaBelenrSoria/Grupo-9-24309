e107f676ddb91db8c22e1639d91c2905ac6b4874
MicaBelenrSoria <micabelenrs@gmail.com>
2024-06-02 19:24:24 -0300

    clase 14: modelos

--- TiendaGrupo9/web/models.py
+++ TiendaGrupo9/web/models.py
@@ -1,12 +1,48 @@
 from django.db import models
 
 class Producto(models.Model):
-    nombre = models.CharField(max_length=100, verbose_name= "Nombre")
+    nombre = models.CharField(max_length=100, verbose_name= "Nombre del Producto")
     descripcion = models.CharField(max_length=200, verbose_name= "Descripcion")
     precio = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Precio")
     imagen_url = models.URLField(verbose_name="Imagen")
 
- #Crear Migraciones 
- #python manage.py makemigrations web   
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100, verbose_name= "Nombre del cliente")
+    apellido = models.CharField(max_length=100, verbose_name= "Apellido del Cliente")
+    dni = models.IntegerField(verbose_name="DNI del Cliente", unique=True)
+    cliente_id = models.IntegerField(verbose_name="ID del cliente", unique=True)
 
- #Ejecutar migracion
+class Vendedor(models.Model):
+    nombre = models.CharField(max_length=100, verbose_name= "Nombre del Vendedor")
+    apellido = models.CharField(max_length=100, verbose_name= "Apellido del Vendedor")
+    vendedor_id = models.IntegerField(verbose_name="ID del Vendedor", unique=True)
+
+class Pedido(models.Model):
+    cliente_id = models.IntegerField(verbose_name="ID del cliente", unique=True)
+    fecha_entrega = models.DateField(verbose_name="Fecha de Entrega")
+    descripcion_pedido = models.CharField(max_length=200, verbose_name= "Descripcion")
+
+ # MODELO DE DATOS 
+"""
+Producto
+    nombre 
+    descripcion
+    precio
+    imagen
+
+Cliente 
+    nombre 
+    apellido
+    dni
+
+Vendedor 
+    nombre
+    apellido
+    vendedor_id
+
+Pedido
+    cliente_id
+    fecha_entrega 
+    descripcion_pedido 
+    
+"""
