788bffb6f6687986f5f7b93822cc3fe494ad3770
MicaBelenrSoria <micabelenrs@gmail.com>
2024-06-30 17:19:10 -0300

    cambios en el bases eliminacion menu hamburguesa js

--- TiendaGrupo9/web/static/web/styles/scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburger = document.getElementById('hamburger');
-    const navbar = document.getElementById('navbar');
-
-    hamburger.addEventListener('click', function() {
-        navbar.classList.toggle('show');
-    });
-});
